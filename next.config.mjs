cadad0e17a4a291650187ab3453dec5ec2adae18
johnchafi <johnchafi@gmail.com>
2024-09-04 19:38:28 -0400

    unoptimize images

--- next.config.mjs
+++ next.config.mjs
@@ -16,6 +16,9 @@ const nextConfig = {
    
     // Optional: Change the output directory `out` -> `dist`
    distDir: 'dist',
+   images:{
+    unoptimized: true,
+   }
   }
    
   export default nextConfig
